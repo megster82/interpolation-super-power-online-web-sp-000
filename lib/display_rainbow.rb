9a4036c71a0316ebd22a264bb353f4fda88f244c
meaghan jennings <meaghanljennings@gmail.com>
2020-05-20 16:36:38 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
-# Write your #display_rainbow method here
+#Define method display_rainbow that prints out the colors of the rainbow correctly when passed in in order
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+def display_rainbow(colors)
+puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
